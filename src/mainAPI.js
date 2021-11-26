775d20c341aa3a3ff4d83cb0cdde8adec87f2536
Jaime Salgado <jsalg10@gmail.com>
2021-11-26 11:35:23 -0600

    setting up .env and weather API fetch call

---
+++ src/mainAPI.js
@@ -0,0 +1,12 @@
+
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env" });
+
+let URL = "http://api.weatherapi.com/v1"
+document.querySelector("button").addEventListener("click", tellWeather)
+
+
+function tellWeather() {
+
+
+}
